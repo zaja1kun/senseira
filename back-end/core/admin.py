c4577b22c6b069df2c591ad5d86607aaad64617f
Zaitsev Yury <zaja1kun@gmail.com>
2016-03-04 17:48:55 +0300

    SE-12 Implement core models displaying in admin interface Minor changes to core model structure

--- back-end/core/admin.py
+++ back-end/core/admin.py
@@ -1,3 +1,66 @@
 from django.contrib import admin
+from .models import Student, Teacher, Subject, Group, Task, Variant
 
-# Register your models here.
+
+class SubjectInline(admin.TabularInline):
+    model = Subject
+    extra = 0
+
+
+class VariantInline(admin.TabularInline):
+    model = Variant
+    extra = 0
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['user', ]}),
+    ]
+
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['user', ]}),
+    ]
+    inlines = [SubjectInline, ]
+    list_display = ('__str__', )
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['group_name', 'monitor']}),
+        (Student._meta.verbose_name_plural, {'fields': ['students', ]}),
+        (Subject._meta.verbose_name_plural, {'fields': ['get_subjects', ]}),
+    ]
+    list_display = ('group_name', 'monitor')
+    readonly_fields = ['get_subjects', ]
+
+    def get_subjects(self, obj):
+        return obj.get_subjects()
+    get_subjects.allow_tags = True
+    get_subjects.short_description = ''
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name', 'subject', 'description']}),
+    ]
+    inlines = [VariantInline]
+    list_display = ('name', 'get_group', 'subject', 'get_teacher', 'get_description_preview')
+    readonly_fields = ['get_description_preview', 'get_teacher', 'get_group']
+
+    def get_description_preview(self, obj):
+        return obj.get_description_preview()
+    get_description_preview.short_description = Task._meta.get_field('description').verbose_name
+
+    def get_teacher(self, obj):
+        return obj.get_teacher()
+    get_teacher.short_description = Teacher._meta.verbose_name
+
+    def get_group(self, obj):
+        return obj.get_group()
+    get_group.short_description = Group._meta.verbose_name
--- back-end/core/models.py
+++ back-end/core/models.py
@@ -3,54 +3,94 @@ from django.contrib.auth.models import User
 
 
 class Student(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, verbose_name=User._meta.verbose_name)
 
     def __str__(self):
-        name = self.user.first_name + ' ' + self.user.last_name
+        name = '{0} {1}'.format(self.user.first_name, self.user.last_name)
         return name if len(name) > 1 else self.user.__str__()
+
+    class Meta:
+        verbose_name = "студент"
+        verbose_name_plural = "студенты"
 
 
 class Teacher(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, verbose_name=User._meta.verbose_name)
 
     def __str__(self):
-        name = self.user.first_name + ' ' + self.user.last_name
+        name = '{0} {1}'.format(self.user.first_name, self.user.last_name)
         return name if len(name) > 1 else self.user.__str__()
 
-
-class Subject(models.Model):
-    name = models.CharField(max_length=30)
-    teacher = models.ForeignKey(Teacher)
-    # group = models.ForeignKey(Group)
-
-    def __str__(self):
-        return self.name
+    class Meta:
+        verbose_name = "преподаватель"
+        verbose_name_plural = "преподаватели"
 
 
 class Group(models.Model):
-    group_name = models.CharField(max_length=10)
-    students = models.ManyToManyField(Student, blank=True)
-    monitor = models.ForeignKey(Student, related_name='group_monitor')
-    subjects = models.ManyToManyField(Subject, blank=True)
+    group_name = models.CharField(max_length=10, verbose_name="номер группы")
+    students = models.ManyToManyField(Student, blank=True, verbose_name=Student._meta.verbose_name_plural)
+    monitor = models.ForeignKey(Student, related_name='group_monitor', verbose_name="староста")
 
     def __str__(self):
         return self.group_name
 
+    def get_subjects(self):
+        return ''.join(['<p>{0} ({1})</p>'.format(subject.name, subject.teacher.__str__()) for subject in Subject.objects.filter(group=self)])
 
-class Task(models.Model):
-    name = models.CharField(max_length=30)
-    subject = models.ForeignKey(Subject)
-    description = models.TextField(blank=True)
+    class Meta:
+        verbose_name = "группа"
+        verbose_name_plural = "группы"
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=30, verbose_name="название")
+    teacher = models.ForeignKey(Teacher, verbose_name=Teacher._meta.verbose_name)
+    group = models.ForeignKey(Group, verbose_name=Group._meta.verbose_name)
 
     def __str__(self):
         return self.name
 
+    def get_full_name(self):
+        return '{0} {1} - {2}'.format(self.group.group_name, self.name, self.teacher.__str__())
 
-class Variant(models.Model):
-    task = models.ForeignKey(Task)
-    description = models.TextField(blank=True)
-    student = models.ForeignKey(Student)
-    is_passed = models.BooleanField()
+    class Meta:
+        verbose_name = "предмет"
+        verbose_name_plural = "предметы"
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=200, verbose_name="название")
+    subject = models.ForeignKey(Subject, verbose_name=Subject._meta.verbose_name)
+    description = models.TextField(blank=True, verbose_name="описание")
 
     def __str__(self):
-        return self.student + ' | ' + self.task
+        return self.name
+
+    def get_description_preview(self):
+        if len(self.description) > 50:
+            return '{0}...'.format(self.description[:50])
+        return self.description
+
+    def get_teacher(self):
+        return self.subject.teacher
+
+    def get_group(self):
+        return self.subject.group
+
+    class Meta:
+        verbose_name = "задание"
+        verbose_name_plural = "задания"
+
+
+class Variant(models.Model):
+    task = models.ForeignKey(Task, verbose_name=Task._meta.verbose_name)
+    description = models.TextField(blank=True, verbose_name="описание")
+    student = models.ForeignKey(Student, verbose_name=Student._meta.verbose_name)
+    is_passed = models.BooleanField(verbose_name="сдано")
+
+    def __str__(self):
+        return self.student.__str__()
+
+    class Meta:
+        verbose_name = "вариант"
+        verbose_name_plural = "варианты"
