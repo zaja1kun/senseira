f291da9eff8ee662cf5f79ed37630d86cae51082
alex <buzuma_leha@mail.ru>
2016-03-19 19:39:25 +0300

    SE-19 Implement of server-side validation of registration forms

--- back-end/register/static/scripts/senseira/insidePasswordChanging.js
+++ back-end/register/static/scripts/senseira/insidePasswordChanging.js
@@ -1,5 +1,6 @@
 var Senseira = Senseira || {};
 Senseira.constructors = Senseira.constructors || {};
+Senseira.constants = Senseira.constants || {};
 
 (function(ko, $) {
     'use strict';
@@ -15,10 +16,40 @@ Senseira.constructors = Senseira.constructors || {};
         self.passwordConfirmation = ko.observable('');
 
         self.isValid = function() {
-            return true;
+            return self.currentPassword().length > 0 &&
+                   self.newPassword().length > 0 &&
+                   self.passwordConfirmation().length > 0 &&
+                   self.currentPassword().length >= Senseira.constants.MinPasswordLength &&
+                   self.newPassword().length >= Senseira.constants.MinPasswordLength &&
+                   self.passwordConfirmation().length >= Senseira.constants.MinPasswordLength &&
+                   self.newPassword() === self.passwordConfirmation();
         };
 
         self.showValidationErrors = function() {
+            var errors = [];
+
+            if (self.currentPassword().length === 0) {
+                errors.push(Senseira.constants.PasswordRequiredMessage);
+            }
+
+            if (self.newPassword().length === 0) {
+                errors.push(Senseira.constants.NewPasswordRequiredMessage);
+            }
+
+            if (self.passwordConfirmation().length === 0) {
+                errors.push(Senseira.constants.PasswordConfirmationRequiredMessage);
+            }
+
+            if (self.currentPassword().length < Senseira.constants.MinPasswordLength ||
+                self.newPassword().length < Senseira.constants.MinPasswordLength ||
+                self.passwordConfirmation().length < Senseira.constants.MinPasswordLength) {
+                errors.push(Senseira.constants.MinPasswordLengthMessage);
+            }
+
+            if (self.passwordConfirmation() !== self.newPassword()){
+                errors.push(Senseira.constants.PasswordsEqualityRequiredMessage);
+            }
+            self.validationErrors(errors);
         };
     }
 
--- back-end/register/static/scripts/senseira/signUp.js
+++ back-end/register/static/scripts/senseira/signUp.js
@@ -1,5 +1,6 @@
 var Senseira = Senseira || {};
 Senseira.constructors = Senseira.constructors || {};
+Senseira.constants = Senseira.constants || {};
 
 (function(ko, $) {
     'use strict';
@@ -20,10 +21,53 @@ Senseira.constructors = Senseira.constructors || {};
         self.rememberMe = ko.observable(false);
 
         self.isValid = function() {
-            return true;
+            return self.name().length > 0 &&
+                   self.surname().length > 0 &&
+                   self.email().length > 0 &&
+                   self.login().length > 0 &&
+                   self.password().length > 0 &&
+                   self.password().length >= Senseira.constants.MinPasswordLength &&
+                   self.passwordConfirmation().length > 0 &&
+                   self.passwordConfirmation().length >= Senseira.constants.MinPasswordLength &&
+                   self.password() === self.passwordConfirmation()
         };
 
         self.showValidationErrors = function() {
+            var errors = [];
+
+            if (self.name().length === 0) {
+                errors.push(Senseira.constants.NameRequiredMessage);
+            }
+
+            if (self.surname().length === 0) {
+                errors.push(Senseira.constants.SurnameRequiredMessage);
+            }
+
+            if (self.email().length === 0) {
+                errors.push(Senseira.constants.EmailRequiredMessage);
+            }
+
+            if (self.login().length === 0) {
+                errors.push(Senseira.constants.LoginRequiredMessage);
+            }
+
+            if (self.password().length === 0) {
+                errors.push(Senseira.constants.PasswordRequiredMessage);
+            }
+
+            if (self.passwordConfirmation().length === 0) {
+                errors.push(Senseira.constants.PasswordConfirmationRequiredMessage);
+            }
+
+            if (self.password().length < Senseira.constants.MinPasswordLength ||
+                self.passwordConfirmation().length < Senseira.constants.MinPasswordLength) {
+                errors.push(Senseira.constants.MinPasswordLengthMessage);
+            }
+
+            if (self.password() !== self.passwordConfirmation()) {
+                errors.push(Senseira.constants.PasswordsEqualityRequiredMessage);
+            }
+            self.validationErrors(errors);
         };
     }
 
